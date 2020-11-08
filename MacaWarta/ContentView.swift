f58ab705757a7b4988217f0ea13793de850a6991
Prima Santosa <mauliawan@primasantosa.com>
2020-11-08 15:14:30 +0700

    Initial Commit

---
+++ MacaWarta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacaWarta
+//
+//  Created by Prima Santosa on 08/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacaWarta/MacaWartaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacaWartaApp.swift
+//  MacaWarta
+//
+//  Created by Prima Santosa on 08/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MacaWartaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
